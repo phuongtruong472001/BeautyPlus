51dd317b7dcacee30bca1f5325bf515315d9a6ef
DESKTOP-H1KK6UA\vanan <vananh059a@gmail.com>
2022-06-18 21:54:04 +0700

    nav bar

--- BeautyPlusAdmin/js/navbar.js
+++ BeautyPlusAdmin/js/navbar.js
@@ -4,7 +4,8 @@ const data = [{
     height: 20,
     position: "-33px -1630px",
     activePosition: "-33px -1587px",
-    title: "Quản Lý tài khoản"
+    title: "Quản Lý tài khoản",
+    url:"google.com"
 },
 {
 
@@ -35,81 +36,81 @@ const data = [{
     activePosition: "-206px -1588px",
     title: "Thống kê"
 },
-// {
-//     width: 14,
-//     height: 17,
-//     position: "-254px -1632px",
-//     activePosition: "-254px -1589px",
-//     title: "Quản lý hoá đơn"
-// },
-// {
-//     width: 18,
-//     height: 17,
-//     position: "-294px -1632px",
-//     activePosition: "-294px -1589px",
-//     title: "kho"
-// },
-// {
-//     width: 18,
-//     height: 17,
-//     position: "-339px -1633px",
-//     activePosition: "-339px -1590px",
-//     title: "Công cụ dụng cụ"
-// },
-// {
-//     width: 16,
-//     height: 16,
-//     position: "-382px -1632px",
-//     activePosition: "-382px -1589px",
-//     title: "Tài sản cố định"
-// },
-// {
-//     width: 17,
-//     height: 18,
-//     position: "-427px -1634px",
-//     activePosition: "-427px -1591px",
-//     title: "Thuế"
-// },
-// {
-//     width: 17,
-//     height: 17,
-//     position: "-471px -1633px",
-//     activePosition: "-471px -1590px",
-//     title: "Giá thành"
-// },
-// {
-//     width: 15,
-//     height: 16,
-//     position: "-514px -1632px",
-//     activePosition: "-514px -1590px",
-//     title: "Tổng hợp"
-// },
-// {
-//     width: 16,
-//     height: 16,
-//     position: "-382px -1664px",
-//     activePosition: "-427px -1664px",
-//     title: "Ngân sách"
-// },
-// {
-//     width: 14,
-//     height: 13,
-//     position: "-552px -1636px",
-//     activePosition: "-552px -1593px",
-//     title: "Báo cáo"
-// },
-// {
-//     width: 17,
-//     height: 14,
-//     position: "-207px -1667px",
-//     activePosition: "-251px -1667px",
-//     title: "Phân tích tài chính"
-// }
+    // {
+    //     width: 14,
+    //     height: 17,
+    //     position: "-254px -1632px",
+    //     activePosition: "-254px -1589px",
+    //     title: "Quản lý hoá đơn"
+    // },
+    // {
+    //     width: 18,
+    //     height: 17,
+    //     position: "-294px -1632px",
+    //     activePosition: "-294px -1589px",
+    //     title: "kho"
+    // },
+    // {
+    //     width: 18,
+    //     height: 17,
+    //     position: "-339px -1633px",
+    //     activePosition: "-339px -1590px",
+    //     title: "Công cụ dụng cụ"
+    // },
+    // {
+    //     width: 16,
+    //     height: 16,
+    //     position: "-382px -1632px",
+    //     activePosition: "-382px -1589px",
+    //     title: "Tài sản cố định"
+    // },
+    // {
+    //     width: 17,
+    //     height: 18,
+    //     position: "-427px -1634px",
+    //     activePosition: "-427px -1591px",
+    //     title: "Thuế"
+    // },
+    // {
+    //     width: 17,
+    //     height: 17,
+    //     position: "-471px -1633px",
+    //     activePosition: "-471px -1590px",
+    //     title: "Giá thành"
+    // },
+    // {
+    //     width: 15,
+    //     height: 16,
+    //     position: "-514px -1632px",
+    //     activePosition: "-514px -1590px",
+    //     title: "Tổng hợp"
+    // },
+    // {
+    //     width: 16,
+    //     height: 16,
+    //     position: "-382px -1664px",
+    //     activePosition: "-427px -1664px",
+    //     title: "Ngân sách"
+    // },
+    // {
+    //     width: 14,
+    //     height: 13,
+    //     position: "-552px -1636px",
+    //     activePosition: "-552px -1593px",
+    //     title: "Báo cáo"
+    // },
+    // {
+    //     width: 17,
+    //     height: 14,
+    //     position: "-207px -1667px",
+    //     activePosition: "-251px -1667px",
+    //     title: "Phân tích tài chính"
+    // }
 
 ];
-const className=".navbar .menu-item-list";
+const className = ".navbar .menu-item-list";
 $(window).on('load', function (e) {
-    const navbar= new NabBar(data,className)
+    const navbar = new NabBar(data, className)
     navbar.render();
     navbar.setUpEvents();
 });
@@ -126,11 +127,13 @@ class NabBar {
     render() {
         this.data.forEach((value, index) => {
             $(this.className)
-                .append(`<div class="menu-item ${index}-th">
-                <div></div>
-                <div style="background-position: ${value.position}; width:${value.width}px; height:${value.height}px;"></div>
-                <div>${value.title}</div>
-            </div>`);
+                .append(`<a href=${value.url}> 
+                        <div class="menu-item ${index}-th">
+                        <div></div>
+                        <div style="background-position: ${value.position}; width:${value.width}px; height:${value.height}px;"></div>
+                        <div>${value.title}</div>
+                        </div>
+                </a>`);
 
         })
     }
