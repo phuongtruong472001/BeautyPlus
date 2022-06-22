8d819768f9015c1b9e592505a4e8e28553452a1e
DESKTOP-H1KK6UA\vanan <vananh059a@gmail.com>
2022-06-22 21:48:12 +0700

    fix thống kê lần cuối

--- BeautyPlusAdmin/statistical.php
+++ BeautyPlusAdmin/statistical.php
@@ -1,4 +1,10 @@
-
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+}
+include_once('connectDB.php');
+?>
 <?php 
     include './statistical.html';
   
